f66c85c00985e1bc80acf29e2b39a295b668bbda
yayeeu <mrayele@gmail.com>
2024-03-14 00:12:44 +0200

    Reviewed entities.  Added missing columns.
    Reviewed entities. All looks good. Added missing columns in ContactLog

--- backend/src/entities/contactLog.entity.ts
+++ backend/src/entities/contactLog.entity.ts
@@ -1,30 +1,55 @@
 import {
-    Entity,
-    Column,
-    ManyToOne,
-    JoinColumn,
-    PrimaryGeneratedColumn,
-    CreateDateColumn,
-    UpdateDateColumn,
-  } from 'typeorm';
+  Entity,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import { Member } from './member.entity';
-  
-  @Entity()
-  export class ContactLog {
-    @PrimaryGeneratedColumn()
-    id: number;
-  
-    @ManyToOne(() => Member, (minister) => minister.members) // source (member who contacts)
-    @JoinColumn({ name: 'minister_id' })
-    minister: Member;
 
-    @ManyToOne(() => Member, (member) => member.ministers) // target (member being contacted)
-    @JoinColumn({ name: 'member_id' })
-    member: Member;
 
-    @CreateDateColumn()
-    created_at: Date;
-  
-    @UpdateDateColumn()
-    updated_at: Date;
-  }
+export enum ContactType {
+textMessage = 'Text Message',
+faceToFace = 'Fact to Face',
+}
+
+@Entity()
+export class ContactLog {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Member, (minister) => minister.members) // source (member who contacts)
+  @JoinColumn({ name: 'minister_id' })
+  minister: Member;
+
+  @ManyToOne(() => Member, (member) => member.ministers) // target (member being contacted)
+  @JoinColumn({ name: 'member_id' })
+  member: Member;
+
+  @Column({
+    name: 'contact_type',
+    type: 'enum',
+    enum: ContactType,
+  })
+  contactType: ContactType;
+
+  @Column({
+    name: 'notes',
+    nullable: true,
+  })
+  notes: string;
+
+  @Column({
+    name: 'flagged',
+    default: true,
+  })
+  flagged: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
